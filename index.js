2d9ca7f7871a0c7022065963ad781b62b96d6406
kauews19 <126421369+kauews19@users.noreply.github.com>
2023-03-03 18:48:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js")
+
+const config = require("./config.json")
+
+const client = new Discord.Client({ 
+  intents: [ 
+Discord.GatewayIntentBits.Guilds
+       ]
+    });
+
+module.exports = client
+
+client.on('interactionCreate', (interaction) => {
+
+  if(interaction.type === Discord.InteractionType.ApplicationCommand){
+
+      const cmd = client.slashCommands.get(interaction.commandName);
+
+      if (!cmd) return interaction.reply(`Error`);
+
+      interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
+
+      cmd.run(client, interaction)
+
+   }
+})
+
+client.on('ready', () => {
+  console.log(`🔥 Estou online em ${client.user.username}!`)
+})
+
+
+client.slashCommands = new Discord.Collection()
+
+require('./handler')(client)
+
+client.login(config.token)
+
+const { QuickDB } = require("quick.db")
+const db = new QuickDB(); // npm i quick.db better-sqlite3
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  let confirm = await db.get(`antilink_${message.guild.id}`);
+  if (confirm === false || confirm === null) {
+    return;
+  } else if (confirm === true) {
+    if (message.member.permissions.has(Discord.PermissionFlagsBits.Administrator)) return; // Caso o usuário tenha permissão de ADM, o bot vai permitir que o mesmo envie links
+    if (message.content.toLocaleLowerCase().includes("http")) {
+      message.delete()
+      message.channel.send(`${message.author} Não envie links no servidor!`)
+    }
+
+  }
+})
